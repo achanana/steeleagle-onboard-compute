635df8d52d3361934b27b84c9650e0d74e640036
Aditya Chanana <achanana@cs.cmu.edu>
2024-11-20 19:49:46 -0500

    Updates to compute engine

--- src/onboard_compute_engine.cpp
+++ src/onboard_compute_engine.cpp
@@ -9,14 +9,18 @@
 #include <modal_pipe_server.h>
 #include <modal_start_stop.h>
 
+#include <chrono>
 #include <functional>
 #include <memory>
+#include <utility>
 #include <vector>
 #include <zmq.hpp>
 
-#define SERVER_NAME "steeleagle-os-onboard-compute"
-#define PIPE_NAME "onboard-compute"
+#define PROCESS_NAME "steeleagle-os-onboard-compute"
+#define PIPE_NAME "onboardcompute"
 #define PIPE_LOCATION (MODAL_PIPE_DEFAULT_BASE_DIR PIPE_NAME "/")
+#define TFLITE_PIPE_NAME "tflite_data"
+#define TFLITE_PIPE_LOCATION (MODAL_PIPE_DEFAULT_BASE_DIR TFLITE_PIPE_NAME "/")
 
 //-----------------------------------------------------------------------------
 
@@ -29,17 +33,39 @@ unique_ptr<ComputeEngine> engine;
 
 //-----------------------------------------------------------------------------
 
-void ComputeEngine::SendResult(const vector<ai_detection_t>& detections) {
+void ComputeEngine::AccumulateResults(vector<ai_detection_t>&& new_detections) {
+    // Check for delimiter frame
+    bool send_results = new_detections.back().frame_id == -1;
+    accumulated_results.insert(accumulated_results.end(),
+                               make_move_iterator(new_detections.begin()),
+                               make_move_iterator(new_detections.end()));
+    if (send_results) {
+        SendResult();
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+void ComputeEngine::SendResult() {
     ComputeResult compute_result;
-    for (const ai_detection_t& detection : detections) {
+    cout << "Sending " << accumulated_results.size() - 1 << " results to client" << endl;
+    for (const ai_detection_t& detection : accumulated_results) {
+        // Skip delimiter frame
+        if (detection.frame_id == -1)
+            continue;
+        cout << "Detection from frame " << detection.frame_id << endl;
         AIDetection detection_proto;
+
+        string class_name(detection.class_name);
+        string cam(detection.cam);
+        cout << "Class name: " << class_name << "; cam: " << cam << endl;
 
         // Set protobuf fields
         detection_proto.set_timestamp_ns(detection.timestamp_ns);
         detection_proto.set_class_id(detection.class_id);
         detection_proto.set_frame_id(detection.frame_id);
-        detection_proto.set_class_name(detection.class_name);
-        detection_proto.set_cam(detection.cam);
+        detection_proto.set_class_name(class_name);
+        detection_proto.set_cam(cam);
         detection_proto.set_class_confidence(detection.class_confidence);
         detection_proto.set_detection_confidence(detection.detection_confidence);
         detection_proto.set_x_min(detection.x_min);
@@ -49,12 +75,20 @@ void ComputeEngine::SendResult(const vector<ai_detection_t>& detections) {
 
         *(compute_result.add_compute_result()) = detection_proto;
     }
+    accumulated_results.clear();
 
     // Send results to client
-    cout << "Sending results to client" << endl;
+    cout << "Sending result(s) to client" << endl;
     string serialized_msg;
     compute_result.SerializeToString(&serialized_msg);
     s_send(socket, serialized_msg);
+
+    // Ready for next client request
+    {
+        lock_guard<mutex> lock(mtx);
+        ready = true;
+    }
+    cv.notify_one();
     // zmq::message_t reply(serialized_msg.size());
     //memcpy(reply.data(), serialized_msg.data(), serialized_msg.size());
 
@@ -63,20 +97,38 @@ void ComputeEngine::SendResult(const vector<ai_detection_t>& detections) {
 
 //-----------------------------------------------------------------------------
 
-ComputeEngine::ComputeEngine(const string& address, int channel) :
+ComputeEngine::ComputeEngine(
+    const string& address,
+    int server_channel,
+    int client_channel) :
     context(1),
     socket(context, ZMQ_REP),
-    channel(channel) {
+    server_channel(server_channel),
+    client_channel(client_channel) {
 
+    cout << "Binding on address " << address << endl;
     socket.bind(address);
 }
 
 //-----------------------------------------------------------------------------
 
 void ComputeEngine::HandleRequest() {
-    cout << "Waiting for request from client" << endl;
+    cout << "\nWaiting for request from client" << endl;
     // Wait for next request from client
-    string client_msg = s_recv(socket);
+
+    zmq::pollitem_t poll_item;
+    poll_item.socket = &socket;
+    poll_item.events = ZMQ_POLLIN;
+
+    zmq::poll(&poll_item, 1, -1);
+
+    string client_msg;
+    if (poll_item.revents & ZMQ_POLLIN) {
+        client_msg = s_recv(socket);
+    } else {
+        cout << "Poller returned prematurely" << endl;
+        return;
+    }
 
     ComputeRequest request;
     if (!request.ParseFromString(client_msg)) {
@@ -89,15 +141,26 @@ void ComputeEngine::HandleRequest() {
 
     camera_image_metadata_t cam_meta;
     cam_meta.magic_number = CAMERA_MAGIC_NUMBER;
+    cam_meta.frame_id = ++frame_id;
     cam_meta.width = request.frame_width();
     cam_meta.height = request.frame_height();
     cam_meta.size_bytes = frame_bytes.size();
     cam_meta.format = IMAGE_FORMAT_YUV422;
 
-    pipe_server_write(channel, &cam_meta, sizeof(camera_image_metadata_t));
-    pipe_server_write(channel, frame_bytes.data(), frame_bytes.size());
+    // pipe_server_write(server_channel, &cam_meta,
+    //                   sizeof(camera_image_metadata_t));
+    // pipe_server_write(server_channel, frame_bytes.data(), frame_bytes.size());
+    if (pipe_server_write_camera_frame(server_channel, cam_meta, frame_bytes.data())) {
+        cerr << "Error writing camera frame to server pipe" << endl;
+    }
 
     cout << "Sent frame to voxl-tflite-server" << endl;
+
+    // Send results back before waiting for next request from client
+    unique_lock<mutex> lock(mtx);
+    ready = false;
+
+    while (!cv.wait_for(lock, chrono::seconds(1), [&] { return ready || !main_running; }));
 }
 
 //-----------------------------------------------------------------------------
@@ -105,12 +168,15 @@ void ComputeEngine::HandleRequest() {
 static void tflite_server_cb(int ch, char *data, int bytes, void *context) {
     cout << "Received results from voxl-tflite-server" << endl;
     vector<ai_detection_t> detections;
-    for (int off = 0; off + (int)sizeof(ai_detection_t) <= bytes; off += sizeof(ai_detection_t)) {
-        ai_detection_t *detection = reinterpret_cast<ai_detection_t *>(data + off);
+
+    for (int off = 0; off + (int)sizeof(ai_detection_t) <= bytes;
+            off += sizeof(ai_detection_t)) {
+        ai_detection_t *detection =
+            reinterpret_cast<ai_detection_t *>(data + off);
         printf("Class confidence: %f\n", detection->class_confidence);
         detections.push_back(*detection);
     }
-    engine->SendResult(detections);
+    engine->AccumulateResults(move(detections));
 }
 
 //-----------------------------------------------------------------------------
@@ -118,7 +184,7 @@ static void tflite_server_cb(int ch, char *data, int bytes, void *context) {
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         cerr << "Expected at least two args\n";
-        exit(-1);
+        return -1;
     }
 
     string host(argv[1]);
@@ -129,61 +195,69 @@ int main(int argc, char *argv[]) {
 
     cout << "ip address is " << oss.str() << endl;
 
-    if (kill_existing_process(SERVER_NAME, 2.0) < -2) return -1;
+    if (kill_existing_process(PROCESS_NAME, 2.0) < -2) return -1;
 
     // start signal handler so we can exit cleanly
     if (enable_signal_handler() == -1) {
         fprintf(stderr, "ERROR: failed to start signal handler\n");
         return -1;
     }
+    make_pid_file(PROCESS_NAME);
 
-    // Open client pipe
-    int flags = CLIENT_FLAG_EN_SIMPLE_HELPER;
-    const string& input_pipe = "run/mpa/tflite_data";
-    int ret = pipe_client_open(0, input_pipe.c_str(), SERVER_NAME, flags, 10 * sizeof(ai_detection_t));
+    int server_ch = pipe_client_get_next_available_channel();
+    int client_ch = pipe_client_get_next_available_channel();
 
-    if (ret) {
-        cerr << "Error opening channel" << endl;
-        pipe_print_error(ret);
-        if (ret == PIPE_ERROR_SERVER_NOT_AVAILABLE) {
-            cerr << "Server not available" << endl;
-        }
+    engine = make_unique<ComputeEngine>(oss.str(), server_ch, client_ch);
+    pipe_client_set_simple_helper_cb(client_ch, tflite_server_cb, nullptr);
+
+    if (create_server_pipe(server_ch)) {
+        cerr << "Failed to create server pipe" << endl;
         return -1;
     }
 
-    make_pid_file(SERVER_NAME);
-
-    int ch = pipe_client_get_next_available_channel();
-    engine = make_unique<ComputeEngine>(oss.str(), ch);
-    pipe_client_set_simple_helper_cb(ch, tflite_server_cb, nullptr);
-
-    if (create_pipe(ch)) {
-        cerr << "Failed to create pipe" << endl;
+    if (create_client_pipe(client_ch)) {
+        cerr << "Failed to create client pipe" << endl;
         return -1;
     }
+
+    main_running = 1;
 
     while (main_running) {
         engine->HandleRequest();
     }
 
     printf("Starting shutdown sequence\n");
+    pipe_client_flush(client_ch);
     pipe_server_close_all();
-    remove_pid_file(SERVER_NAME);
+    remove_pid_file(PROCESS_NAME);
     printf("exiting cleanly\n");
     return 0;
 }
 
 //-----------------------------------------------------------------------------
 
-static int create_pipe(int ch) {
-    int flags = SERVER_FLAG_EN_CONTROL_PIPE;
+static int create_server_pipe(int ch) {
     pipe_info_t info = {
-        PIPE_NAME, PIPE_LOCATION, "bytes", SERVER_NAME, MODAL_PIPE_DEFAULT_PIPE_SIZE };
+        PIPE_NAME, PIPE_LOCATION, "camera", PROCESS_NAME,
+        16 * MODAL_PIPE_DEFAULT_PIPE_SIZE, 0
+    };
 
-    if (!pipe_server_create(ch, info, flags)) {
+    if (pipe_server_create(ch, info, 0)) {
         return -1;
     }
     return 0;
+}
+
+//-----------------------------------------------------------------------------
+
+static int create_client_pipe(int ch) {
+    int ret = pipe_client_open(ch, TFLITE_PIPE_LOCATION, PROCESS_NAME,
+                               CLIENT_FLAG_EN_SIMPLE_HELPER,
+                               10 * sizeof(ai_detection_t));
+    if (ret) {
+        pipe_print_error(ret);
+    }
+    return ret;
 }
 
 //-----------------------------------------------------------------------------
--- src/onboard_compute_engine.h
+++ src/onboard_compute_engine.h
@@ -1,22 +1,32 @@
 #include <ai_detection.h>
 
+#include <condition_variable>
+#include <mutex>
 #include <string>
 
 #include "zmq.hpp"
 
 using namespace std;
 
-static int create_pipe(int ch);
+static int create_server_pipe(int ch);
+static int create_client_pipe(int ch);
 
 class ComputeEngine {
  public:
-    ComputeEngine(const string& address, int channel);
+    ComputeEngine(const string& address, int server_channel, int client_channel);
     void HandleRequest();
     void TfliteServerCb(int ch, char *data, int bytes, void *context);
-    void SendResult(const vector<ai_detection_t>& detections);
+    void SendResult();
+    void AccumulateResults(vector<ai_detection_t>&& new_detections);
 
  private:
+    int frame_id = 0;
     zmq::context_t context;
     zmq::socket_t socket;
-    int channel;
+    int server_channel;
+    int client_channel;
+    mutex mtx;
+    condition_variable cv;
+    bool ready;
+    vector<ai_detection_t> accumulated_results;
 };
